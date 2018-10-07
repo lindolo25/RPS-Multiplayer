c9b64b8d216cb5396e2f5ee5dea098660231fbae
lindolo25 <lindolo25@outlook.com>
2018-10-07 13:05:08 -0400

    player dynamic is complete, players can join and leave is there is a spot available.

--- js/main.js
+++ js/main.js
@@ -1,91 +1,106 @@
 var rpsGame = 
 {
-    players: null,
+    players: [],
     currentPlayer: null,
-    get currentPlayerKey() { if(this.currentPlayer === null) return null; else return this.currentPlayer.substring(0, this.currentPlayer.indexOf("/")); },
-    get areYouInTheGame() {
-        if(this.currentPlayer === null) return false;
-        else if(this.players === null) return false;
-        else 
-        {
-            var player1 = "player1/" + this.players.player1.name;
-            var player2 = "player2/" + this.players.player2.name;
-            if(this.currentPlayer === player1) return true;
-            else if(this.currentPlayer === player2) return true;
-            else return false;
-        }
+    get arePlayersAvailable() {
+        if(this.players.length < 2) return true;
+        else return false;
+    },
+    get currentPlayerIndex()
+    {
+        return this.getPlayerIndex(this.currentPlayer);
+    },
+    getPlayerIndex: function(playerId)
+    {
+        return rpsGame.players.findIndex(i => i.id === playerId.key);
     },
     init: function()
     {
-        this.currentPlayer = localStorage.getItem("rpsGamePlayer");
-        console.log(this.currentPlayer);
-        palyers = firebase.database().ref("players/");
-        palyers.on("value", this.playersChanged);
+        // initialize database event handlers ----------------------------------------
+        var palyers = firebase.database().ref("players/");
+        palyers.on("child_added", this.playerJoined);
 
+        var palyers = firebase.database().ref("players/");
+        palyers.on("child_removed", this.playerLeft);
 
-        $("#join-game").on("click", function()
-        { 
-            var modal = '<div id="join-game-modal" class="container-fluid">\
-                    <div class="row justify-content-sm-center">\
-                        <div class="col-12 col-sm-8 col-md-4">\
-                            <form id="join-game-modal-form">\
-                                <div class="form-group">\
-                                    <input type="input" class="form-control" id="join-game-input" aria-describedby="emailHelp" placeholder="Type in your name">\
-                                </div>\
-                                <button type="submit" class="btn btn-primary" id="join-game-submit">Submit</button>\
-                            </form>\
-                        </div>\
-                    </div>\
-                </div>';
+        var palyers = firebase.database().ref("players/");
+        palyers.on("child_changed", this.playerChanged);
 
-            $(modal).appendTo("body");
-            $("#join-game-submit").on("click", function(event) 
-            {
-                event.preventDefault();
-                var name = $("#join-game-input").val().trim();
-                rpsGame.joinTheGame(name);
-                $("#join-game-modal").remove();
-            })
+        // show jumbotron, modify depending player availability -----------------------
+        $("#players-area").hide();
+        $("#join-game-form").hide();
+
+        // suscrive join-game-submit click event --------------------------------------
+        $("#join-game-submit").on("click", function(event) 
+        {
+            event.preventDefault();
+            var name = $("#join-game-input").val().trim();
+            $("#join-game-input").val("");
+            if(name === "" || name === null) return;
+
+            rpsGame.joinTheGame(name);
         });
     },
     joinTheGame(username)
     {
-        var player = null;
-        if(rpsGame.players.player1.available)
+        if(rpsGame.arePlayersAvailable && rpsGame.currentPlayer === null)
         {
-            player = firebase.database().ref("players/player1/");
-        }
-        else
-        {
-            player = firebase.database().ref("players/player2/");
-        }        
-        this.currentPlayer = player.key + "/" + username;
-        player.set({ 
-            name: username,
-            wins: 0,
-            lose: 0,
-            currentPlay: null,
-            available: false
-        });
-        localStorage.setItem("rpsGamePlayer", this.currentPlayer);
-    },
-    playersChanged: function(snapshot)
-    {
-        rpsGame.players = snapshot.val();
-        console.log(rpsGame.players);
+            var player = firebase.database().ref("players/").push();
+            rpsGame.currentPlayer = player.key;
+            player.set({
+                name: username,
+                wins: 0,
+                lose: 0,
+                id: player.key
+            });
 
-        if(rpsGame.areYouInTheGame)
-        {
-            console.log("you are in the game");
+            $("#welcome").hide();
+            $("#players-area").show();
         }
-        else { console.log("no current player"); }
+        
     },
     leaveTheGame: function()
     {
-        localStorage.removeItem("rpsGamePlayer");
-        player = firebase.database().ref("players/" + this.currentPlayerKey);
-        this.currentPlayer = null;
-        player.set({ available: true });
-    }
+        if(rpsGame.currentPlayer !== null)
+        {
+            console.log("leaving");
+            firebase.database().ref("players/" + this.currentPlayer).remove();
+        }        
+    },
+    playerChanged: function(snapshot)
+    {
+        var i = rpsGame.getPlayerIndex(snapshot.key);
+        rpsGame.players[i] = snapshot.val();
+        console.log(rpsGame.players);
+    },
+    playerJoined: function(snapshot)
+    {
+        rpsGame.players.push(snapshot.val());
+        console.log(rpsGame.players);
 
+        if(rpsGame.currentPlayer !== null) return;
+
+        rpsGame.toggleWelcomeForm();
+    },
+    playerLeft: function(snapshot)
+    {
+        var i = rpsGame.getPlayerIndex(snapshot.key);
+        rpsGame.players.splice(i, 1);
+        console.log(rpsGame.players);
+
+        if(rpsGame.currentPlayer !== null) return;
+
+        this.toggleWelcomeForm();
+    },
+    toggleWelcomeForm: function()
+    {
+        if(!rpsGame.arePlayersAvailable)
+        {
+            $("#join-game-form").hide();
+        }
+        else
+        {
+            $("#join-game-form").show();
+        }
+    }
 };
